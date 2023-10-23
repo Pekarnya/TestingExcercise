b1e8c0b9437307e00a50f73fc9e53c8fe9f89140
Tckachencko Grigorij <tckachencko.grigorij@yandex.ru>
2023-10-23 20:57:29 +0300

    feat(tree-alghorythm): add tree node class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('Heledaddddflo');
+type TreeNode = {
+    id: number | string;
+    parent: number | null | undefined | string;
+    type?: string | null;
+};
+
+export default class TreeStore {
+    private tree: Array<TreeNode>;
+    constructor(items: Array<TreeNode>) {
+        this.tree = items;
+    }
+    getAll(): Array<TreeNode> {
+        return this.tree;
+    }
+}
+
+const items = [
+    { id: 1, parent: 'root' },
+    { id: 2, parent: 1, type: 'test' },
+    { id: 3, parent: 1, type: 'test' },
+
+    { id: 4, parent: 2, type: 'test' },
+    { id: 5, parent: 2, type: 'test' },
+    { id: 6, parent: 2, type: 'test' },
+
+    { id: 7, parent: 4, type: null },
+    { id: 8, parent: 4, type: null }
+];
+const treeNode = new TreeStore(items);
+console.log(treeNode instanceof TreeStore);
