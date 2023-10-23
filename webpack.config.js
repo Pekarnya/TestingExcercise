49069a57067a7d803d0bba24b45dfaacf4f3cd60
Tckachencko Grigorij <tckachencko.grigorij@yandex.ru>
2023-10-23 18:47:25 +0300

    feat(testing_excercise): first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  watch: true,
+};
